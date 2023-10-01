f89b0ab48fde78774ab72949acc36ff1a21344f4
yechenyu <2684096975@qq.com>
2023-10-01 11:42:48 +0800

    fix:修改工厂模式的代码，同时补充gitignore信息

--- src/factorymethod/EnviormentEnum.java
+++ src/factorymethod/EnviormentEnum.java
@@ -1,6 +1,7 @@
 package factorymethod;
 
 import java.util.Objects;
+import java.util.function.Supplier;
 
 /**
  * @Author: ye.ChenYu
@@ -8,19 +9,19 @@ import java.util.Objects;
  * @Description: 环境枚举，用于模拟工厂模式
  */
 public enum EnviormentEnum {
-    DEV("dev",new DevelopmentEnv()),
-    PROD("prod",new ProductEnv()),
-    SIT("sit",new SystemIntegrationTestEnv()),
-    UAT("uat",new UserAcceptanceTestEnv())
+    DEV("dev", DevelopmentEnv::new),
+    PROD("prod",ProductEnv::new),
+    SIT("sit",SystemIntegrationTestEnv::new),
+    UAT("uat",UserAcceptanceTestEnv::new)
     ;
 
     private final String envName;
 
 
 
-    private final AbstractEnv abstractEnv;
+    private final Supplier<AbstractEnv> abstractEnv;
 
-    EnviormentEnum(String envName, AbstractEnv abstractEnv) {
+    EnviormentEnum(String envName, Supplier<AbstractEnv> abstractEnv) {
         this.envName = envName;
         this.abstractEnv = abstractEnv;
     }
@@ -31,7 +32,7 @@ public enum EnviormentEnum {
         }
         for (EnviormentEnum value : EnviormentEnum.values()) {
             if (value.envName.equals(envName)) {
-                return value.abstractEnv;
+                return value.abstractEnv.get();
             }
         }
         throw new RuntimeException("没有找到这个环境！");
